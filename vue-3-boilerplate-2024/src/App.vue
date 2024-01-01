cbfb67904c86030314b5badef31f77a07e06e22e
suhcodes <contact@suh.codes>
2024-01-01 01:45:53 -0300

    :tada: inits 2024 vue boilerplate

---
+++ vue-3-boilerplate-2024/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Navbar from '@/components/DNavbar.vue'
+</script>
+
+<template>
+  <Navbar />
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ vue-3-boilerplate-2024/src/components/DNavbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav class="flex m-4 justify-between">
+    <div class="flex gap-2">
+      <RouterLink v-for="route in routes" :to="route.path" :key="route">{{ route.name }}</RouterLink>
+    </div>
+    <div>{{ firstName }}</div>
+  </nav>
+</template>
+
+<script setup>
+import { ref, computed, onBeforeMount } from 'vue'
+import { useRouter } from 'vue-router'
+import { useAuthStore } from '@/stores/authStore'
+const router = useRouter()
+const routes = ref(router.getRoutes())
+const authStore = useAuthStore()
+onBeforeMount(() => {
+	authStore.authUser()
+})
+const firstName = computed(() => authStore?.firstName)
+</script>
+
+
+<style scoped></style>
