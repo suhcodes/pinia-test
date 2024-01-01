c34a55334859cc8ef8815a27227a9fcb05f69f35
suhcodes <contact@suh.codes>
2024-01-01 12:46:39 -0300

    🔥 removes project

--- src/views/Profile.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="max-w-3xl mx-auto p-12">
-    <h3 class="text-center mb-4">
-      PROFILE page!
-    </h3>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos dolores iste, et est, repellendus eius in quas vel beatae corporis quis officiis perspiciatis totam, reprehenderit magni exercitationem. Error, quia officia.</p>
-  </div>
-</template>
