5d7a5a14fdbde73199f8bc07d93e688a2f9d7358
suhcodes <contact@suh.codes>
2024-01-01 12:49:59 -0300

    :tada: init project

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const Home = () => import('../views/HomeView.vue')
+const Dashboard = () => import('../views/DashboardView.vue')
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: Dashboard
+    }
+  ]
+})
+
+export default router
