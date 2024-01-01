a6dc96a3356329755f3254d2520903525a8e994b
suhcodes <contact@suh.codes>
2024-01-01 14:29:47 -0300

    ➕ Adds axios dependency

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,9 +2,9 @@
   <div class="max-w-lg mx-auto my-12">
     <h1 class="text-center mb-4 dasboard-header">This is a Welcome page!</h1>
     <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla officia nobis accusamus
-      corrupti molestias vero sit numquam libero. Veritatis iusto, pariatur fuga eum quod odit sed
-      velit nostrum earum commodi.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla officia
+      nobis accusamus corrupti molestias vero sit numquam libero. Veritatis
+      iusto, pariatur fuga eum quod odit sed velit nostrum earum commodi.
     </p>
   </div>
 </template>
